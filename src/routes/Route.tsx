6644087a4dabe2d3aac11daaef64080816b01862
aoi-256 <aoi256jp@Gmail.com>
2025-09-18 10:42:10 +0900

    ページタイトルの変更

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -4,6 +4,9 @@ import ProtectedRoute from './ProtectedRoute'
 import Login from '../LoginPage'
 import TestPage from '../pages/Documents/TestPage'
 
+
+// 各ページへのルーティング設定
+// ProtectedRoute でログイントークンが切れている場合はログインページにリダイレクトする
 export default function AppRoutes() {
 
     return (
